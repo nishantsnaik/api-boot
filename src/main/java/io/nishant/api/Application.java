8abd98b8eb4419c3a0cc7674c8afc074daf6b30d
Nishant Naik <>
2018-07-30 18:09:27 -0400

    docker commits

--- src/main/java/io/nishant/api/Application.java
+++ src/main/java/io/nishant/api/Application.java
@@ -3,18 +3,19 @@ package io.nishant.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.AbstractEnvironment;
 
 import io.nishant.api.config.SwaggerConfig;
 import io.nishant.api.config.WebConfig;
 
 @SpringBootApplication
+@Profile(value="prod")
 @Import({ WebConfig.class, SwaggerConfig.class })
 public class Application {
 
 	public static void main(String[] args) {
-		// System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME,
-		// "prod");
+		//System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME,"prod");
 		SpringApplication.run(Application.class, args);
 	}
 }
